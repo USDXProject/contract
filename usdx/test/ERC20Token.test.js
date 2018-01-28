7452c1e9048db1df57f26662076fcd363e834dcc
cpeng <pengcheng85@gmail.com>
2018-01-28 17:09:08 -0500

    Introduce EventVerifier for events verifications. Also expanded test coverage for ERC20Token contract.

--- usdx/test/ERC20Token.test.js
+++ usdx/test/ERC20Token.test.js
@@ -3,6 +3,7 @@
 const TestERC20Token = artifacts.require('TestERC20Token.sol');
 const BigNumber = web3.BigNumber;
 const utils = require('./helpers/Utils');
+const EventVerifier = require('./helpers/EventVerifier');
 const expect = require('chai').expect;
 
 require('chai')
@@ -20,73 +21,56 @@ contract('ERC20Token', (accounts) => {
     });
     it('verifies the token name after construction', async () => {
         let name = await token.name.call();
-        assert.equal(name, 'TestERC20');
+        name.should.equal('TestERC20');
     });
 
     it('verifies the token symbol after construction', async () => {
         let symbol = await token.symbol.call();
-        assert.equal(symbol, 'TERC');
+        symbol.should.equal('TERC');
     });
 
     it('verifies the balances after a transfer', async () => {
         const result = await token.transfer(accounts[1], 500);
         let balance;
         balance = await token.balanceOf.call(accounts[0]);
-        assert.equal(balance, 1500);
+        balance.should.be.bignumber.equal(1500);
         balance = await token.balanceOf.call(accounts[1]);
-        assert.equal(balance, 500);
+        balance.should.be.bignumber.equal(500);
 
-        let transferEvent = result.logs.find(e => e.event === 'Transfer');
-        expect(transferEvent).to.exist;
-        transferEvent.args._from.should.equal(accounts[0]);
-        transferEvent.args._to.should.equal(accounts[1]);
-        transferEvent.args._value.should.be.bignumber.equal(500);
+        EventVerifier.transferEvent(result, accounts[0], accounts[1], 500);
     });
 
     it('verifies that a transfer fires a Transfer event', async () => {
-        let res = await token.transfer(accounts[1], 500);
-        assert(res.logs.length > 0 && res.logs[0].event == 'Transfer');
+        let result = await token.transfer(accounts[1], 500);
+        EventVerifier.transferEvent(result, accounts[0], accounts[1], 500);
     });
 
     it('should throw when attempting to transfer more than the balance', async () => {
-        try {
-            await token.transfer(accounts[1], 5000);
-            assert(false, "didn't throw");
-        }
-        catch (error) {
-            return utils.ensureException(error);
-        }
+        await token.transfer(accounts[1], 5000)
+            .should.be.rejectedWith(utils.revert);
     });
 
     it('should throw when attempting to transfer to an invalid address', async () => {
-        try {
-            await token.transfer(invalidAccount, 10);
-            assert(false, "didn't throw");
-        }
-        catch (error) {
-            return utils.ensureException(error);
-        }
+        await token.transfer(invalidAccount, 10)
+            .should.be.rejectedWith(utils.revert);
     });
 
     it('verifies the allowance after an approval', async () => {
-        await token.approve(accounts[1], 500);
-        let allowance = await token.allowance.call(accounts[0], accounts[1]);
-        assert.equal(allowance, 500);
+        let result = await token.approve(accounts[1], 500);
+        let balance = await token.allowance.call(accounts[0], accounts[1]);
+        balance.should.be.bignumber.equal(500);
+
+        EventVerifier.approvalEvent(result, accounts[0], accounts[1], 500);
     });
 
     it('verifies that an approval fires an Approval event', async () => {
-        let res = await token.approve(accounts[1], 500);
-        assert(res.logs.length > 0 && res.logs[0].event == 'Approval');
+        let result = await token.approve(accounts[1], 500);
+        EventVerifier.approvalEvent(result, accounts[0], accounts[1], 500);
     });
 
     it('should throw when attempting to define allowance for an invalid address', async () => {
-        try {
-            await token.approve(invalidAccount, 10);
-            assert(false, "didn't throw");
-        }
-        catch (error) {
-            return utils.ensureException(error);
-        }
+        await token.approve(invalidAccount, 10)
+            .should.be.rejectedWith(utils.revert);
     });
 
     it('verifies the balances after transferring from another account', async () => {
@@ -94,59 +78,42 @@ contract('ERC20Token', (accounts) => {
         await token.transferFrom(accounts[0], accounts[2], 50, { from: accounts[1] });
         let balance;
         balance = await token.balanceOf.call(accounts[0]);
-        assert.equal(balance, 1950);
+        balance.should.be.bignumber.equal(1950);
         balance = await token.balanceOf.call(accounts[1]);
-        assert.equal(balance, 0);
+        balance.should.be.bignumber.equal(0);
         balance = await token.balanceOf.call(accounts[2]);
-        assert.equal(balance, 50);
+        balance.should.be.bignumber.equal(50);
     });
 
     it('verifies that transferring from another account fires a Transfer event', async () => {
-        await token.approve(accounts[1], 500);
-        let res = await token.transferFrom(accounts[0], accounts[2], 50, { from: accounts[1] });
-        assert(res.logs.length > 0 && res.logs[0].event == 'Transfer');
+        let approveResult = await token.approve(accounts[1], 500);
+        let transferResult = await token.transferFrom(accounts[0], accounts[2], 50, { from: accounts[1] });
+        EventVerifier.approvalEvent(approveResult, accounts[0], accounts[1], 500);
+        EventVerifier.transferEvent(transferResult, accounts[0], accounts[2], 50);
     });
 
     it('verifies the new allowance after transferring from another account', async () => {
         await token.approve(accounts[1], 500);
         await token.transferFrom(accounts[0], accounts[2], 50, { from: accounts[1] });
         let allowance = await token.allowance.call(accounts[0], accounts[1]);
-        assert.equal(allowance, 450);
+        allowance.should.be.bignumber.equal(450);
     });
 
     it('should throw when attempting to transfer from another account more than the allowance', async () => {
         await token.approve(accounts[1], 100);
-
-        try {
-            await token.transferFrom(accounts[0], accounts[2], 200, { from: accounts[1] });
-            assert(false, "didn't throw");
-        }
-        catch (error) {
-            return utils.ensureException(error);
-        }
+        await token.transferFrom(accounts[0], accounts[2], 200, { from: accounts[1] })
+            .should.be.rejectedWith(utils.revert);
     });
 
     it('should throw when attempting to transfer from an invalid account', async () => {
         await token.approve(accounts[1], 100);
-
-        try {
-            await token.transferFrom(invalidAccount, accounts[2], 50, { from: accounts[1] });
-            assert(false, "didn't throw");
-        }
-        catch (error) {
-            return utils.ensureException(error);
-        }
+        await token.transferFrom(invalidAccount, accounts[2], 50, { from: accounts[1] })
+            .should.be.rejectedWith(utils.revert);
     });
 
     it('should throw when attempting to transfer from to an invalid account', async () => {
         await token.approve(accounts[1], 100);
-
-        try {
-            await token.transferFrom(accounts[0], invalidAccount, 50, { from: accounts[1] });
-            assert(false, "didn't throw");
-        }
-        catch (error) {
-            return utils.ensureException(error);
-        }
+        await token.transferFrom(accounts[0], invalidAccount, 50, { from: accounts[1] })
+            .should.be.rejectedWith(utils.revert);
     });
 });
