37822bfba652dd2d24dec22c9481fdbaccdf02fd
cpeng <pengcheng85@gmail.com>
2018-02-24 23:23:07 -0500

    Introduce USDXVesting as a separate contract and its corresponding tests for various scenarios.

--- usdx/migrations/2_initial_migration.js
+++ usdx/migrations/2_initial_migration.js
@@ -1,10 +1,12 @@
-
 let CrowdsaleController = artifacts.require("CrowdsaleController.sol");
+let USDXToken = artifacts.require("USDXToken.sol");
 let config = require('../config/config')(web3);
 const TestERC20Token = artifacts.require('./helper/TestERC20Token.sol');
 const TestBurnableToken = artifacts.require('./helper/TestBurnableToken.sol');
+
 module.exports = function(deployer) {
   deployer.deploy(CrowdsaleController,config.startBlock,config.endBlock,config.initialExchangeRate,config.presaleAmount,config.founderPercentOfTotal,config.name,config.symbol,config.decimals);
+  deployer.deploy(USDXToken, config.name, config.symbol, config.decimals);
   deployer.deploy(TestERC20Token);
   deployer.deploy(TestBurnableToken);
 };
